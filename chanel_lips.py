c7de87bf675eb8517752bd42859062f7835b0df9
jyFengGoGo <15801766361@163.com>
2020-04-28 23:16:18 -0400

    final version

--- chanel_lips.py
+++ chanel_lips.py
@@ -5,8 +5,11 @@ import sqlite3
 import time
 import json
 
+
 CACHE_FILENAME = 'chanel_cache.json'
 CACHE_DICT = {}
+
+local_root = 'http://127.0.0.1:5000'
 
 root = "https://www.chanel.com"
 BASEURL = "https://www.chanel.com/us/"
@@ -14,7 +17,7 @@ BASEURL = "https://www.chanel.com/us/"
 # get urls of all categories
 def get_all_categories():
     resp = request_with_cache(BASEURL)
-    soup = BeautifulSoup(resp.text, 'html.parser')
+    soup = BeautifulSoup(resp, 'html.parser')
     all_4_ul = soup.find('ul', class_='header__primary__links header__primary__links2')
     all_makeup_li = all_4_ul.find_all('li', class_='js-header-entry ')
     all_makeup_li = all_makeup_li[2]
@@ -35,7 +38,7 @@ def get_all_categories():
 # get product urls
 def get_products_url(category_url):
     resp = request_with_cache(category_url)
-    soup = BeautifulSoup(resp.text, 'html.parser')
+    soup = BeautifulSoup(resp, 'html.parser')
     contain = soup.find('div', class_='js-main-content plp')
     grid = contain.find('div', class_='pdp-grid js-pdp-grid')
     list = grid.find('div', class_='product-grid columns-mobile-2 columns-tablet-2 columns-desktop-3')
@@ -56,7 +59,7 @@ def get_product_information(product_url):
     product_info["product"] = {}
 
     resp = request_with_cache(product_url)
-    soup = BeautifulSoup(resp.text, 'html.parser')
+    soup = BeautifulSoup(resp, 'html.parser')
     contain = soup.find('div', class_='js-main-content pdp')
     text = contain.find('div', class_='product-details__text')
     name = text.find('span', class_='heading product-details__title ')
@@ -182,15 +185,12 @@ def insert_db(product_info, category):
     conn.close()
     return
 
-def build_db(lips_categories):
-    for i, (category, url) in enumerate(lips_categories.items()):
-        print(category)
-        prod_urls = get_products_url(lips_categories[category])
-        for url in prod_urls:
-            print(url)
-            info = get_product_information(url)
-            insert_db(info, category)
-            time.sleep(2)
+def build_db(category, category_url):
+    prod_urls = get_products_url(category_url)
+    for url in prod_urls:
+        info = get_product_information(url)
+        insert_db(info, category)
+        time.sleep(2)
 
 
 def open_cache():
@@ -240,17 +240,165 @@ def request_with_cache(url):
         resp = CACHE_DICT[url]
     else:
         print("fetching")
-        resp = requests.get(url)
+        resp = requests.get(url).text
         CACHE_DICT[url] = resp
     return resp
 
 
-if __name__ == "__main__":
-    CACHE_DICT = open_cache()
+def search_basic_prod_info(category):
+    conn = sqlite3.connect('chanel_lips.sqlite')
+    cur = conn.cursor()
+    search_sql_query = '''
+                    SELECT Id, ProductName, Price, ShadeNum, Rating 
+                    FROM Product
+                    WHERE CategoryName = "{}"
+                          '''.format(category)
+    basic_prod_info = cur.execute(search_sql_query).fetchall()
+    return basic_prod_info
 
+
+def search_detail_prod_info(productId, mode):
+    conn = sqlite3.connect('chanel_lips.sqlite')
+    cur = conn.cursor()
+    if mode == 'detail':
+        search_sql_query = '''
+                            SELECT * FROM Product
+                            WHERE Id = {}
+                                  '''.format(productId)
+        prod_info = cur.execute(search_sql_query).fetchall()
+    elif mode == 'name':
+        search_sql_query = '''
+                            SELECT ProductName FROM Product
+                            WHERE Id = {}
+                                  '''.format(productId)
+        prod_info = cur.execute(search_sql_query).fetchall()
+    return prod_info
+
+
+def search_reviews(productId):
+    conn = sqlite3.connect('chanel_lips.sqlite')
+    cur = conn.cursor()
+    search_sql_query = '''
+                            SELECT ReviewBody, ReviewRating, dateCreated 
+                            FROM Review
+                            WHERE ProductId = {}
+                                  '''.format(productId)
+    review_info = cur.execute(search_sql_query).fetchall()
+    return review_info
+
+def generate_id_list(query_result, dim=0):
+    id_list = []
+    for tup in query_result:
+        id_list.append(tup[dim])
+    return id_list
+
+
+def generate_basic_prod_info(category):
+    basic_prod_info = search_basic_prod_info(category)
+    id_list = generate_id_list(basic_prod_info)
+    basic_prod_info.insert(0, ('Id', 'Product Name', 'Price', 'Shades Number', 'Overall Rating'))
+    print_query_result(basic_prod_info)
+    return id_list
+
+
+def print_query_result(raw_query_result):
+    ''' Pretty prints raw query result
+
+    Parameters
+    ----------
+    raw_query_result
+        a list of tuples that represent raw query result
+
+    Returns
+    -------
+    None
+    '''
+
+    for tup in raw_query_result:
+        row = ''
+        for info in tup:
+            if isinstance(info, int):
+                block = "{}".format(info)
+                row += "{:<21}".format(block)
+                continue
+            if isinstance(info, float):
+                block = "{}".format(info)
+                row += "{:<21}".format(block)
+                continue
+            if len(info) > 20:
+                block = info[:17] + '... '
+                row += "{:<21}".format(block)
+            else:
+                block = info
+                row += "{:<21}".format(block)
+
+        print(row)
+    print('\n')
+    pass
+
+
+def generate_local_link(productId):
+    link = local_root + '/product/' + productId
+    print('Open this page in your browser to view product details: ' + link)
+    return
+
+def interactive_prompt():
+    '''main logic to interact with users,
+        prompt the user for input and display the results
+
+    Parameters
+    ----------
+    lips_categories: dict
+        dict of all categories and their urls
+
+    Returns
+    -------
+    None
+    '''
+    print("Initializing......")
     create_db()
     lips_categories = get_all_categories()
-    build_db(lips_categories)
+    category_ls = lips_categories.keys()
+    print("Ready to go!")
 
+    category_in_db = []
+
+    exit = False
+    print("Welcome to explore lips product from CHANEL!")
+    while not exit:
+        str = ', '
+        print("Categories available for check: {}. ".format(str.join(category_ls)))
+        category = input("Please input product category to view products (eg. Lipstick), or 'exit': ")
+        if category in category_ls:
+            if category not in category_in_db:
+                build_db(category, lips_categories[category])
+            id_list = generate_basic_prod_info(category)
+            back = False
+            while not back and not exit:
+                productId = input("Input listed product id to check more information, or 'back' to check other categories, or exit': ")
+                if productId.isnumeric() and int(productId) in id_list:
+                    generate_local_link(productId)
+                elif productId == 'exit':
+                    print("Bye!")
+                    exit = True
+                elif productId =='back':
+                    back = True
+                    continue
+                else:
+                    print("Invalid Id. Try again.")
+                    continue
+        elif category == 'exit':
+            print("Bye!")
+            exit = True
+        else:
+            print("Invalid category! Try again.")
+            continue
+    return
+
+
+if __name__ == "__main__":
+    CACHE_DICT = open_cache()
+    interactive_prompt()
     save_cache(CACHE_DICT)
+
 
